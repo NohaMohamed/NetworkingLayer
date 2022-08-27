62021ca8ae6fd96f372294a627583558dd696216
Noha Mohamed <nohamohamedali1993@gmail.com>
2022-08-28 01:39:26 +0200

    handle failure in case of caching

--- Sources/NetworkingLayer/APICleint.swift
+++ Sources/NetworkingLayer/APICleint.swift
@@ -73,7 +73,9 @@ extension APICleint: APICleintProtocol {
     }
     func getCachedResponse(request: URLRequest, completion: @escaping (Result<Data, CustomNetworkError>) -> Void) {
         let data = urlCache.cachedResponse(for: request)?.data
-        guard let decodeResult = data else { return }
+        guard let data = data else {
+            completion(.failure(.generic)
+            return }
         completion(.success(decodeResult))
         
     }
